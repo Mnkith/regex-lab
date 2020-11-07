c6443e220e8acacc1095459598f4c1b87fd61564
Mnkith <munkithabid@gmail.com>
2020-11-06 22:50:34 -0600

    commit tree dde9d014a265fc0dd1cb164b0d4849cadb0e0680

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
 def starts_with_a_vowel?(word)
-
+  /\b[aioueAEIOU]/ === word
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  /\A[A-Z].*[?!.:;]\Z/ === text
+  # text.match(/\A[A-Z].*[?!.:;]\Z/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  /(\d{10}|\d{3} \d{3} \d{4}|\(\d{3}\)\d{3}-?\d{4})/ === phone #the very first ? followed by : is to turn off capturing
+                                                            #groups since we don't need them
 end
